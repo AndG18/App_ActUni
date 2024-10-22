38753e603b6ac2e323da9d28b2b289c2031aeca6
Monarca <gilgarciaandresfelipe092@gmail.com>
2024-10-21 19:02:23 -0500

    third commit

--- app/src/main/java/com/example/app_actuni/MainActivity.java
+++ app/src/main/java/com/example/app_actuni/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app_actuni;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+  /*  private EditText et1Is, et2Cis; */
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistroCliente.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/app_actuni/RegistroCliente.java
@@ -0,0 +1,14 @@
+package com.example.app_actuni;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistroCliente extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro_cliente);
+
+    }
+}
